f0b857334bd8f4608166c010e30ea89cfca72a38
Mateus Varnier <varnier.c.mateus@gmail.com>
2023-06-03 22:06:06 -0300

    Projeto final

---
+++ config/database.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise;
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/test', {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log('Conectado ao MongoDB'))
+    .catch((err) => console.log(err))
